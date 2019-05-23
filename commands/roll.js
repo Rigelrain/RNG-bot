a208f2b96c3359bc5426a2ec70bebd54d79bbb78
Sade Sirén <fyrwill@gmail.com>
2019-05-23 19:11:34 +0300

    add modifier functionality, untested

--- commands/roll.js
+++ commands/roll.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'roll',
   description: 'Roll a dice given as parameter.',
   aliases: ['roll', 'dice'],
-  usage: '[die, ex. d20] [number of times to roll, ex x3]',
+  usage: '[die, ex. d20] [optional: number of times to roll, ex x3] [modifier, ex +2 (the plus sign is important!)]',
   args: true,
   execute(message, args) {
     // first arg = the die to roll
@@ -25,18 +25,23 @@ module.exports = {
       }
     }
 
-    // second arg is the number of times to roll
+    // second arg is the number of times to roll or the modifier
     const times = args.shift()
     let timestoroll = 1
     let result = 0
+    let modifier = 0
 
     if(times) {
       let usertimestoroll = parseInt(times)
-      if(!usertimestoroll) {
-        // user did not give a number of times to roll, check for 'x'
-        usertimestoroll = parseInt(times.substring(1))
-        if(usertimestoroll) {
-          timestoroll = usertimestoroll
+      if(!usertimestoroll) { // check for 'x' or '+'
+        if(times.substring(0,1) == '+') { // this is a modifier
+          modifier = parseInt(times.substring(1))
+        }
+        else { // it is a multiplier of dice
+          usertimestoroll = parseInt(times.substring(1))
+          if(usertimestoroll) {
+            timestoroll = usertimestoroll
+          }
         }
       }
       else {
@@ -50,6 +55,27 @@ module.exports = {
       timestoroll--
     }
 
-    return message.reply(`Rollin D${rollnumber}! ... Got: ${result}`)    
+    // third is the modifier, unless gotten previously
+    if(!modifier || modifier > 0) { // modifier is already handled
+      // do nothing
+    }
+    else {
+      modifier = args.shift()
+      let usermodifier = parseInt(modifier)
+      if(!usermodifier) {
+        // check for '+'
+        usermodifier = parseInt(modifier.substring(1))
+        if(usermodifier) {
+          modifier = usermodifier
+        }
+        else {
+          modifier = 0
+        }
+      }
+    }
+
+    result += modifier
+
+    return message.reply(`Rollin D${rollnumber}! ... And with modifier ${modifier} you got: ${result}`)
   },
 }
